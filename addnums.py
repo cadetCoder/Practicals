438ce1394298b817cb682717a5c91b1d2aac5f90
Darrel Toledo <darreltoledo@icloud.com>
2024-02-06 18:42:28 +0000

    Add files via upload

---
+++ addnums.py
@@ -0,0 +1,6 @@
+num1 = input ("Please enter a whole number: ")
+num2 = input ("Please enter another whole number: ")
+num1 = int (num1)
+num2 = int (num2)
+total = num1 + num2
+print ("Adding these two numbers gives", total)
